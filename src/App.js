4dc293aa4e80d601c6bdb34442cf7dce2bc89bf7
Anton G Neuhold Jr <agneuhold@gmail.com>
2019-12-06 11:53:54 -0800

    Add in metal styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import './components/ServiceRow';
 import ServiceRow from './components/ServiceRow';
+import ChromeDivider from './components/ChromeDivider';
 import Footer from './components/Footer';
 import logo from './resources/logo.jpg';
 function App() {
@@ -18,21 +19,26 @@ function App() {
       <section>
 
         {/* This will be a section for each service offering */}
+        <ChromeDivider/>
         <ServiceRow 
           serviceTitle="Truck Mount Carpet Steam Cleaning" 
           serviceDescription="Test Description"
           whiteOnBlack={true}
         />
+        <ChromeDivider/>
         <ServiceRow 
           serviceTitle="Encapsulation Carpet Cleaning" 
           serviceDescription="Test Description"
           whiteOnBlack={false}
         />
+        <ChromeDivider/>
         <ServiceRow 
           serviceTitle="Tile & Grout Restoration This is a longer title" 
           serviceDescription="Test Description"
           whiteOnBlack={true}
         />
+        <ChromeDivider/>
+        
 
       </section>
       <Footer/>
