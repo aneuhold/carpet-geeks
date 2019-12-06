ab3450cb732bdad05a24a590cb6855865aec8a7a
Anton G Neuhold Jr <agneuhold@gmail.com>
2019-12-06 11:57:33 -0800

    Add call to action

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './components/ServiceRow';
 import ServiceRow from './components/ServiceRow';
 import ChromeDivider from './components/ChromeDivider';
+import CallToAction from './components/CallToAction';
 import Footer from './components/Footer';
 import logo from './resources/logo.jpg';
 function App() {
@@ -25,21 +26,19 @@ function App() {
           serviceDescription="Test Description"
           whiteOnBlack={true}
         />
-        <ChromeDivider/>
         <ServiceRow 
           serviceTitle="Encapsulation Carpet Cleaning" 
           serviceDescription="Test Description"
           whiteOnBlack={false}
         />
-        <ChromeDivider/>
         <ServiceRow 
           serviceTitle="Tile & Grout Restoration This is a longer title" 
           serviceDescription="Test Description"
           whiteOnBlack={true}
         />
         <ChromeDivider/>
-        
-
+        <CallToAction/>
+        <ChromeDivider/>
       </section>
       <Footer/>
     </div>
