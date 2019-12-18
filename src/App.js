9cdbc210d3d8000feca2e6e0a3f6e47d4e3da242
Anton G Neuhold Jr <agneuhold@gmail.com>
2019-12-17 16:18:46 -0800

    Add motto and some basic refs

--- src/App.js
+++ src/App.js
@@ -7,89 +7,102 @@ import CallToAction from './components/CallToAction';
 import Footer from './components/Footer';
 import logo from './resources/logo.jpg';
 import History from './components/History';
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <img
-          className="header__logo"
-          src={logo}
-          alt="Carpet Geeks Logo"
-        />
-        <span>The best cleaner out there... Or a motto here :)</span>
-      </header>
-      <section>
 
-        {/* This will be a section for each service offering */}
-        <ChromeDivider/>
-        <ServiceRow 
-          serviceTitle="Truck Mount Carpet Steam Cleaning" 
-          serviceDescription="Test Description"
-          whiteOnBlack={true}
-        />
-        <ServiceRow 
-          serviceTitle="Encapsulation Carpet Cleaning" 
-          serviceDescription="Test Description"
-          whiteOnBlack={false}
-        />
-        <ServiceRow 
-          serviceTitle="Tile & Grout Restoration" 
-          serviceDescription="Test Description"
-          whiteOnBlack={true}
-        />
-        <ServiceRow 
-          serviceTitle="Stain Removal" 
-          serviceDescription="Test Description"
-          whiteOnBlack={false}
-        />
-        <ServiceRow 
-          serviceTitle="Medical Sanitizing" 
-          serviceDescription="Test Description"
-          whiteOnBlack={true}
-        />
-        <ServiceRow 
-          serviceTitle="Mildew Treatment" 
-          serviceDescription="Test Description"
-          whiteOnBlack={false}
-        />
-        <ServiceRow 
-          serviceTitle="Pet Stain & Odor Removal" 
-          serviceDescription="Test Description"
-          whiteOnBlack={true}
-        />
-        <ServiceRow 
-          serviceTitle="Vent & Duct Cleaning" 
-          serviceDescription="Test Description"
-          whiteOnBlack={false}
-        />
-        <ServiceRow 
-          serviceTitle="Upholstery and Fine Fabric Cleaning" 
-          serviceDescription="Test Description"
-          whiteOnBlack={true}
-        />
-        <ServiceRow 
-          serviceTitle="Oriental Rug Cleaning" 
-          serviceDescription="Test Description"
-          whiteOnBlack={false}
-        />
-        <ServiceRow 
-          serviceTitle="Upholstery and Fine Fabric Cleaning" 
-          serviceDescription="Test Description"
-          whiteOnBlack={true}
-        />
-        <ServiceRow 
-          serviceTitle="Emergency Water Damage Restoration & Drying Service" 
-          serviceDescription="Test Description"
-          whiteOnBlack={false}
-        />
-        <ChromeDivider/>
-        <CallToAction/>
-        <ChromeDivider/>
-        <History/>
-        <ChromeDivider/>
-      </section>
-      <Footer/>
-    </div>
-  );
+/**
+ * When the page first starts up, the phone number will show up at the bottom. 
+ * After the page reaches the phone number section, it will shrink away. 
+ */
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.callToActionRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <img
+            className="header__logo"
+            src={logo}
+            alt="Carpet Geeks Logo"
+          />
+          <span className="header__motto">Smarter than the dirtiest carpets</span>
+        </header>
+        <section>
+  
+          {/* This will be a section for each service offering */}
+          <ChromeDivider/>
+          <ServiceRow 
+            serviceTitle="Truck Mount Carpet Steam Cleaning" 
+            serviceDescription="Test Description"
+            whiteOnBlack={true}
+          />
+          <ServiceRow 
+            serviceTitle="Encapsulation Carpet Cleaning" 
+            serviceDescription="Test Description"
+            whiteOnBlack={false}
+          />
+          <ServiceRow 
+            serviceTitle="Tile & Grout Restoration" 
+            serviceDescription="Test Description"
+            whiteOnBlack={true}
+          />
+          <ServiceRow 
+            serviceTitle="Stain Removal" 
+            serviceDescription="Test Description"
+            whiteOnBlack={false}
+          />
+          <ServiceRow 
+            serviceTitle="Medical Sanitizing" 
+            serviceDescription="Test Description"
+            whiteOnBlack={true}
+          />
+          <ServiceRow 
+            serviceTitle="Mildew Treatment" 
+            serviceDescription="Test Description"
+            whiteOnBlack={false}
+          />
+          <ServiceRow 
+            serviceTitle="Pet Stain & Odor Removal" 
+            serviceDescription="Test Description"
+            whiteOnBlack={true}
+          />
+          <ServiceRow 
+            serviceTitle="Vent & Duct Cleaning" 
+            serviceDescription="Test Description"
+            whiteOnBlack={false}
+          />
+          <ServiceRow 
+            serviceTitle="Upholstery and Fine Fabric Cleaning" 
+            serviceDescription="Test Description"
+            whiteOnBlack={true}
+          />
+          <ServiceRow 
+            serviceTitle="Oriental Rug Cleaning" 
+            serviceDescription="Test Description"
+            whiteOnBlack={false}
+          />
+          <ServiceRow 
+            serviceTitle="Upholstery and Fine Fabric Cleaning" 
+            serviceDescription="Test Description"
+            whiteOnBlack={true}
+          />
+          <ServiceRow 
+            serviceTitle="Emergency Water Damage Restoration & Drying Service" 
+            serviceDescription="Test Description"
+            whiteOnBlack={false}
+          />
+          <ChromeDivider/>
+          <CallToAction ref={this.callToActionRef}/>
+          <ChromeDivider/>
+          <History/>
+          <ChromeDivider/>
+        </section>
+        <Footer/>
+      </div>
+    );
+  }
+  
 }
 export default App;
